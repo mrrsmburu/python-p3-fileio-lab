57cf37160a13e18e7ee025ff5c578b03119589d4
mrrsmburu <mrrsmburu@gmail.com>
2023-12-19 17:18:24 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
 def write_file(file_name, file_content):
-    pass
+    
+    file_name_with_extension = file_name + ".txt"
 
-def append_file(file_name, append_content):
-    pass
+    
+    with open(file_name_with_extension, 'w') as file:
+        file.write(file_content)
+
+def append_to_file(file_name, file_content):
+    
+    file_name_with_extension = file_name + ".txt"
+
+    with open(file_name_with_extension, 'a') as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    
+    file_name_with_extension = file_name + ".txt"
+
+    
+    with open(file_name_with_extension, 'r') as file:
+        content = file.read()
+        return content
+
